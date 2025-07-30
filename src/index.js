69291a3914d1c2cf79ad134bdc836a4e2f5c1667
Coding <codingmaniac227@gmail.com>
2025-07-30 09:58:13 -0400

    upd: Removed webpack dependencies and updated package.json file to reflect changes

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import './styles.css'
-
-console.log("Webpack is bundling correctly")
-
-document.body.innerHTML = `<h1>Hello Webpack To-Do List!</h1>`
-
-// Adding this for a commit to showcase why i've added folders
